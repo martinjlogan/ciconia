a2b623e4abb7f924bc8f0a394d14669b315a7df8
Martin Logan <martinjlogan@gmail.com>
2011-06-01 23:06:04 -0500

    agner now needs more robust url struct parsing and then the package download

--- lib/agner_driver/src/agner_driver.erl
+++ lib/agner_driver/src/agner_driver.erl
@@ -26,40 +26,69 @@
 %% @spec
 %% @end
 %%--------------------------------------------------------------------
-list(Repos, Timeout) ->
+list(_Repos, Timeout) ->
     AgnerRepos = agner_repo_list(Timeout),
     ?DEBUG("agner repos are ~p~n", [AgnerRepos]),
-    fetch_package_info(AgnerRepos, Timeout).
+    fetch_package_info_for_all_repos(AgnerRepos, Timeout).
 
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+%% Fetch a list of urls for all agner repos in the github based repo
+agner_repo_list(Timeout) ->
+    URL = "http://github.com/api/v2/json/organizations" ++
+	"/agner/public_repositories",
+    ReposJson = get(URL, Timeout),
+    {Obj, _, _} = ktj_parse:parse(ReposJson),
+    {obj,[{<<"repositories">>, ObjList}]} = Obj,
+    lists:map(fun({obj, KeyVals}) ->
+		      {value, {<<"url">>, RepoUrl}} =
+			  lists:keysearch(<<"url">>, 1, KeyVals),
+		      binary_to_list(RepoUrl)
+	      end,
+	      ObjList).
 
-fetch_package_info(AgnerRepos, Timeout) ->
-    lists:foreach(fun(Repo) ->
-			  file:set_cwd("/tmp"),
-			  % TODO make sure git is present
-			  Hash = repo_hash(Repo, Timeout),
-			  timer:sleep(1500),
-			  case Hash of
-			      undefined -> 
-				  io:format("skipping ~s~n", [Repo]);
-			      Hash ->
-				  ConfigURL = project_url(Repo, Hash, Timeout),
-				  AppVersions =
-				      fetch_app_file_versions(ConfigURL,
-							      Timeout),
-				  io:format("~p~n", [AppVersions])
-			  end
-		  end,
-		  AgnerRepos).
+fetch_package_info_for_all_repos([AgnerRepo|T], Timeout) ->
+    AgnerRepoBlobURL = github_all_blob_url(AgnerRepo),
+    ConfigHash = fetch_github_blob_value(<<"agner.config">>, AgnerRepoBlobURL, Timeout),
+    timer:sleep(1500),
+    case ConfigHash of
+	undefined -> 
+	    ?INFO("skipping ~s~n", [AgnerRepo]);
+	ConfigHash ->
+	    AgnerConfigURL = github_hash_based_url(AgnerRepo, ConfigHash),
+	    timer:sleep(1500),
+	    ProjectURLStruct = project_url(AgnerConfigURL, Timeout),
+	    ?DEBUG("project_url ~p~n", [ProjectURLStruct]),
+	    timer:sleep(1500),
+	    %% XXX now need to fetch all versions available and parse the url struct in a more robust way
+	    %% after that I need to store hash urls for the blob set at that version. Then write
+	    %% an http or git based way to pull the full project down on an install. 
+	    AppVersions =
+		fetch_app_file_versions(ProjectURLStruct, Timeout),
+	    ?DEBUG("app versions found ~p~n", [AppVersions])
+    end,
+    fetch_package_info_for_all_repos(T, Timeout);
+fetch_package_info_for_all_repos([], _Timeout) ->
+    [].
+
+github_all_blob_url(Repo) ->
+    UserAndRepo = github_user_and_repo(Repo),
+    "http://github.com/api/v2/json/blob/all/" ++
+	UserAndRepo ++ "/master".
+
+github_hash_based_url(Repo, ConfigHash) when is_binary(ConfigHash) ->
+    github_hash_based_url(Repo, binary_to_list(ConfigHash));
+github_hash_based_url(Repo, ConfigHash) ->
+    UserAndRepo = github_user_and_repo(Repo),
+    "http://github.com/api/v2/json/blob/show/" ++ UserAndRepo ++ "/" ++ ConfigHash.
 
 %% XXX cleanup the algorythem that works here. Make it smoother.
 %% add in support for branches and other repos. Add in more slowdown.
 fetch_app_file_versions({url, {git, Repo, _}}, Timeout) ->
     AppHash = fetch_app_file_hash(Repo, Timeout),
-    AppURL = build_hash_based_url(Repo, AppHash),
+    AppURL = github_hash_based_url(Repo, AppHash),
     AppFileStr = get(AppURL, Timeout),
     {ok, Toks, _} = erl_scan:string(AppFileStr),
     {ok, {application, _Name, Tuples}} = erl_parse:parse_term(Toks),
@@ -69,22 +98,18 @@ fetch_app_file_versions(_, _Timeout) ->
     unsupported_repo_type.
     
 fetch_app_file_hash(Repo, Timeout) ->
-    HashURL = build_hash_url(Repo),
+    HashURL = github_all_blob_url(Repo),
     {Obj, _, _} = ktj_parse:parse(get(HashURL, Timeout)),
     search_blobs(".*\\.app", Obj).
 
-project_url(Repo, Hash, Timeout) ->
-    HashStr = binary_to_list(Hash),
-    ConfigURL =
-	build_hash_based_url(Repo, HashStr),
+project_url(ConfigURL, Timeout) ->
     ConfigStr = get(ConfigURL, Timeout),
-    Config =
-	try 
-	    parse_out_project_repo_url(ConfigStr)
-	catch
-	    _C:_E ->
-		undefined
-	end.
+    try 
+	parse_out_project_repo_url(ConfigStr)
+    catch
+	_C:_E ->
+	    undefined
+    end.
     
 -spec parse_out_project_repo_url(string()) -> term().
 parse_out_project_repo_url(ConfigString) ->
@@ -100,78 +125,11 @@ parse_out_project_repo_url(ConfigString) ->
 	undefined ->
 	    undefined
     end.
-
 	    
-			      
-
-repo_hash(Repo, Timeout) ->
-    HashURL = build_hash_url(Repo),
+fetch_github_blob_value(BlobKeyOrPattern, HashURL, Timeout) ->
     {Obj, _, _} = ktj_parse:parse(get(HashURL, Timeout)),
-    agner_config_hash(Obj).
-    
-build_hash_based_url(Repo, Hash) when is_binary(Hash) ->
-    build_hash_based_url(Repo, binary_to_list(Hash));
-build_hash_based_url(Repo, Hash) ->
-    UserAndRepo = github_user_and_repo(Repo),
-    "http://github.com/api/v2/json/blob/show/" ++ UserAndRepo ++ "/" ++ Hash.
+    search_blobs(BlobKeyOrPattern, Obj).
 
-build_hash_url(Repo) ->
-    UserAndRepo = github_user_and_repo(Repo),
-    "http://github.com/api/v2/json/blob/all/" ++
-	UserAndRepo ++ "/master".
-
-agner_config_hash(Json) ->
-    search_blobs(<<"agner.config">>, Json).
-
-search_blobs(Key, {obj, [{<<"blobs">>, {obj, BlobList}}]})
-  when is_binary(Key) ->
-    case lists:keysearch(Key, 1, BlobList) of
-	{value, {_, Blob}} -> Blob;
-	_ -> undefined
-    end;
-search_blobs(Regexp, {obj, [{<<"blobs">>, {obj, BlobList}}]}) ->
-    search_blogs_by_regexp(Regexp, BlobList);
-search_blobs(_Key, Resp) ->
-    throw(?UEX(bad_response_from_agner_repo,
-	       "bad response from the agner repo~n" ++
-	       "please check that the repo you used is valid~n",
-	       [Resp])).
-
-search_blogs_by_regexp(Regexp, [{Key, Value}|T]) ->
-    case re:run(binary_to_list(Key), Regexp) of
-	{match, _} ->
-	    Value;
-	_ ->
-	    search_blogs_by_regexp(Regexp, T)
-    end;
-search_blogs_by_regexp(_Regexp, []) ->
-    undefined.
-	    
-    
-	
-    
-
-github_user_and_repo([_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_|T]) ->
-    lists:reverse(strip_dot_git(lists:reverse(T))).
-
-strip_dot_git([$t,$i,$g,$.|T]) ->
-    T;
-strip_dot_git(Str) ->
-    Str.
-
-agner_repo_list(Timeout) ->
-    URL = "http://github.com/api/v2/json/organizations" ++
-	"/agner/public_repositories",
-    ReposJson = get(URL, Timeout),
-    {Obj, _, _} = ktj_parse:parse(ReposJson),
-    {obj,[{<<"repositories">>, ObjList}]} = Obj,
-    lists:map(fun({obj, KeyVals}) ->
-		      {value, {<<"url">>, RepoUrl}} =
-			  lists:keysearch(<<"url">>, 1, KeyVals),
-		      binary_to_list(RepoUrl)
-	      end,
-	      ObjList).
-    
 get(URL, Timeout) ->
     try
 	{ok, "200", _, Payload} = ibrowse:send_req(URL, [], get, [], [], Timeout),
@@ -182,3 +140,36 @@ get(URL, Timeout) ->
 		       "Failed on a GET to ~s~nCheck your connection to the network~n",
 		       [URL]))
     end.
+
+search_blobs(Key, {obj, [{<<"blobs">>, {obj, BlobList}}]})
+  when is_binary(Key) ->
+    case lists:keysearch(Key, 1, BlobList) of
+	{value, {_, Blob}} -> Blob;
+	_ -> undefined
+    end;
+search_blobs(Regexp, {obj, [{<<"blobs">>, {obj, BlobList}}]}) ->
+    search_blobs_by_regexp(Regexp, BlobList);
+search_blobs(_Key, Resp) ->
+    throw(?UEX(bad_response_from_agner_repo,
+	       "bad response from the agner repo~n" ++
+	       "please check that the repo you used is valid~n",
+	       [Resp])).
+
+search_blobs_by_regexp(Regexp, [{Key, Value}|T]) ->
+    case re:run(binary_to_list(Key), Regexp) of
+	{match, _} ->
+	    Value;
+	_ ->
+	    search_blobs_by_regexp(Regexp, T)
+    end;
+search_blobs_by_regexp(_Regexp, []) ->
+    undefined.
+
+github_user_and_repo([_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_|T]) ->
+    lists:reverse(strip_dot_git(lists:reverse(T))).
+
+strip_dot_git([$t,$i,$g,$.|T]) ->
+    T;
+strip_dot_git(Str) ->
+    Str.
+
